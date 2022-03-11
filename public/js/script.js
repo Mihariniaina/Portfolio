452676a003e5c0a6d6367782e967b883732e26eb
mihariniaina <r.mmihary@gmail.com>
2022-03-11 23:28:58 +0300

    Initialize translation

---
+++ public/js/script.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * @name:       translatorjs
+ * @version:    3.1.0
+ * @author:     EOussama
+ * @license     MIT
+ * @source:     https://github.com/EOussama/translatorjs
+ * 
+ * A simple javascript library for translating web content.
+ * 
+ */
+
+'use strict';
+
+window.addEventListener('load', () => {
+
+  // Creating a dictionary
+  var dictionary = {
+    'en': {
+      'inform': 'I\'m'
+    },
+    'fr': {
+      'inform': 'Je suis'
+    },
+    'mg': {
+      'inform': 'Izaho dia'
+    }
+  };
+
+  // Creating a translator instance
+  var translator = new EOTranslator(dictionary);
+
+  // Getting the DOM elements
+  var globalTranslate = document.getElementById('globalTranslate');
+
+  // Setting the default language
+  globalTranslate.value = document.documentElement.lang || 'en';
+
+  // Translating the greeting input when the greet button is clicked
+  globalTranslate.addEventListener('change', function () {
+    var language = globalTranslate.value;
+
+    try {
+      translator.translateDOM(document.body, language);
+    } catch (e) {
+      alert(e);
+    }
+  });
+
+  // Invoking the change event
+  globalTranslate.dispatchEvent(new Event('change'));
+});
