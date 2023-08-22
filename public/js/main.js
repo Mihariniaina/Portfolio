18d5a5a014d36bcb8f35a3cce3698cc8da3f4574
Rilah Mario <rilah@adrware.mg>
2023-08-22 18:09:18 +0300

    factor serverhost, add clickInfo, add visitCount

--- public/js/main.js
+++ public/js/main.js
@@ -1,110 +1,134 @@
+var serverhost= "http://localhost:3000/";
+// var serverhost= "https://api.rilah-mario.com/";
+
 (function ($) {
-    "use strict";
-    
-    // loader
-    var loader = function () {
-        setTimeout(function () {
-            if ($('#loader').length > 0) {
-                $('#loader').removeClass('show');
-            }
-        }, 1);
-    };
-    loader();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
-    
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('nav-sticky');
-        } else {
-            $('.navbar').removeClass('nav-sticky');
-        }
-    });
-    
-    
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-    
-    
-    // Typed Initiate
-    if ($('.hero .hero-text h2').length === 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
+  "use strict";
+  updateClickStat("pageReload");
+
+  //get page visit number
+  $.ajax({
+    url: serverhost+"getLineCount",
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+      // The success callback function
+      const pageVisitNumber = data.lineCount;
+      $('#result').text(pageVisitNumber);
+    },
+    error: function(xhr, status, error) {
+      console.error("Error:", error);
     }
-    
-    
-    // Skills
-    $('.skills').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+  });
+
+  // Get the user's IP address using a public API
+  $.getJSON("https://api.ipify.org?format=json", function(data) {
+    var ipAddress = data.ip;
+    var userAgent = navigator.userAgent;
+    updateVisitInfo(ipAddress, userAgent);
+  });
+
+  // loader
+  var loader = function () {
+      setTimeout(function () {
+          if ($('#loader').length > 0) {
+              $('#loader').removeClass('show');
+          }
+      }, 1);
+  };
+  loader();
+  
+  
+  // Initiate the wowjs
+  new WOW().init();
+  
+  
+  // Back to top button
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
+  
+  
+  // Sticky Navbar
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+          $('.navbar').addClass('nav-sticky');
+      } else {
+          $('.navbar').removeClass('nav-sticky');
+      }
+  });
+  
+  
+  // Smooth scrolling on the navbar links
+  $(".navbar-nav a").on('click', function (event) {
+      if (this.hash !== "") {
+          event.preventDefault();
+          
+          $('html, body').animate({
+              scrollTop: $(this.hash).offset().top - 45
+          }, 1500, 'easeInOutExpo');
+          
+          if ($(this).parents('.navbar-nav').length) {
+              $('.navbar-nav .active').removeClass('active');
+              $(this).closest('a').addClass('active');
+          }
+      }
+  });
+  
+  
+  // Typed Initiate
+  if ($('.hero .hero-text h2').length === 1) {
+      var typed_strings = $('.hero .hero-text .typed-text').text();
+      var typed = new Typed('.hero .hero-text h2', {
+          strings: typed_strings.split(', '),
+          typeSpeed: 100,
+          backSpeed: 20,
+          smartBackspace: false,
+          loop: true
+      });
+  }
+  
+  
+  // Skills
+  $('.skills').waypoint(function () {
+      $('.progress .progress-bar').each(function () {
+          $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      });
+  }, {offset: '80%'});
 
 
-    // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            }
-        }
-    });
-    
-    
-    
-    // Portfolio filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
+  // Testimonials carousel
+  $(".testimonials-carousel").owlCarousel({
+      center: true,
+      autoplay: true,
+      dots: true,
+      loop: true,
+      responsive: {
+          0:{
+              items:1
+          }
+      }
+  });
+  
+  // Portfolio filter
+  var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+  });
 
-    $('#portfolio-filter li').on('click', function () {
-        $("#portfolio-filter li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
+  $('#portfolio-filter li').on('click', function () {
+      $("#portfolio-filter li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+      portfolioIsotope.isotope({filter: $(this).data('filter')});
+  });
+  
 })(jQuery);
 
 function sendMail(){
@@ -118,17 +142,14 @@ function sendMail(){
           senderName: senderName,
           senderEmail: senderEmail,
           objectEmail: objectEmail,
-          messageEmail:messageEmail
+          messageEmail: messageEmail
     
         };
         //show loader
         $('#loader').addClass('show');
-        // $.post("http://localhost:3000/sendMail", body, (data, status) => {
-
-        console.log("aaa0");
-
+        updateClickStat('sendMail');
         $.ajax({
-            url: "http://api.rilah-mario.com/sendMail",
+            url: serverhost+"sendMail",
             type: "POST",
             data: body,
             dataType: "json",
@@ -190,3 +211,44 @@ function closeNotification(status){
     })(jQuery);
 }
 
+function updateVisitInfo(ip, userAgent) {
+  const body = {
+    data: ip + "||" + userAgent
+  };
+  $.ajax({
+      type: "POST",
+      url: serverhost+"updateFile",
+      data: body,
+      dataType: "json",
+      success: function(data, response) {
+        console.log("success");
+      },
+      error: function(error) {
+        console.error(error);
+      }
+  });
+}
+
+
+//CLICK COUNTER
+function updateClickStat(element){
+  console.log("update statistic:", element);
+
+  const body = {
+    data: element,
+  };
+
+  //update reload number
+  $.ajax({
+    type: "POST",
+    url: serverhost+"updateClickStat",
+    data: body,
+    dataType: "json",
+    success: function(data, response) {
+      console.log("success");
+    },
+    error: function(error) {
+      console.error(error);
+    }
+  });
+}
